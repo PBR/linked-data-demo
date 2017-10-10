6d8eff7c7d9dbfd55336825986fe206c03cb2ddd
Papoutsoglou <evangelia.papoutsoglou@wur.nl>
2017-10-11 01:14:26 +0200

    Add queries; fix existing ones; add links to accessions

--- js/config.js
+++ js/config.js
@@ -1,35 +1,59 @@
+//var SPARQL_ENDPOINT = 'http://dev-vm.fair-dtls.vm.surfsara.nl:8891/sparql?query='
 var SPARQL_ENDPOINT = 'http://localhost:8890/sparql?default-graph-uri=http://localhost:8890/DAV/home/demo_user/rdf_sink&query='
 
 var TEMPLATE_QUERIES = {
         1 : {
-                text : '[working] Get number of accessions  with a specific phenotype',
+                text : 'Get number of accessions  with a specific phenotype',
                 variables: ['phenotypeVariable', 'phenotypeValue'],
                 query : readFile('sparqlQueries/template1.sparql', 'text')
 
             },
-       2 : {
-                text : '[working] Get number of accessions from a specific country of origin',
-                variables: ['country'],
+        2 : {
+                text : 'Get accessions  with a specific phenotype',
+                variables: ['phenotypeVariable', 'phenotypeValue'],
                 query : readFile('sparqlQueries/template2.sparql', 'text')
 
-           },
-	3 : {
-	  text : '[working] Get number of accessions with a specific phenotype from specific country of origin',
-                variables: ['phenotypeVariable', 'phenotypeValue', 'country'],
+            },
+
+       3 : {
+                text : 'Get number of accessions from a specific country of origin',
+                variables: ['country'],
                 query : readFile('sparqlQueries/template3.sparql', 'text')
 
-            },
-        4 : {
-                text : '[working] Get number of accessions with a specific biological status',
-                variables: ['biologicalStatus'],
+           },
+       4 : {
+                text : 'Get accessions from a specific country of origin',
+                variables: ['country'],
                 query : readFile('sparqlQueries/template4.sparql', 'text')
-            },
 
-        5 : {
-                text : '[?] Get number of accessions with a specific phenotype (from a certain environment: region or specific plant management)',
+           },
+       5 : {
+                text : 'Get number of accessions with a specific phenotype from specific country of origin',
+                variables: ['phenotypeVariable', 'phenotypeValue', 'country'],
+                query : readFile('sparqlQueries/template5.sparql', 'text')
+
+            },
+       6 : {
+                text : 'Get accessions with a specific phenotype from specific country of origin',
+                variables: ['phenotypeVariable', 'phenotypeValue', 'country'],
+                query : readFile('sparqlQueries/template6.sparql', 'text')
+
+            },
+        7 : {
+                text : 'Get number of accessions with a specific biological status',
+                variables: ['biologicalStatus'],
+                query : readFile('sparqlQueries/template7.sparql', 'text')
+            },
+        8 : {
+                text : 'Get accessions with a specific biological status',
+                variables: ['biologicalStatus'],
+                query : readFile('sparqlQueries/template8.sparql', 'text')
+            },
+        /*9 : {
+                text : 'Get number of accessions with a specific phenotype (from a certain environment: region or specific plant management)',
                 variables: ['country'],
                 query : readFile('sparqlQueries/template2.sparql', 'text')
-            }
+            } */
     };
 
 var VARIABLE_QUERIES = {
